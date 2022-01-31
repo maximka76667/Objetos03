c1c81038eebb192cbda397c90be8d6c8ad62f758
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-01-31 10:36:03 +0100

    Initial commit

---
+++ src/ej01_encuesta/Encuesta.java
@@ -0,0 +1,56 @@
+package ej01_encuesta;
+
+public class Encuesta {
+
+	private String pregunta;
+	private int votosA, votosB, total;
+	private double porcentajeA, porcentajeB;
+
+	public Encuesta(String pregunta) {
+		this.pregunta = pregunta;
+		this.votosA = 0;
+		this.votosB = 0;
+		this.total = 0;
+		this.porcentajeA = 0;
+		this.porcentajeB = 0;
+	}
+
+	public void increaseA() {
+		++this.votosA;
+		++this.total;
+		this.porcentajeA = this.total > 0 ? this.votosA * 100.0 / this.total : 0;
+		this.porcentajeB = this.total > 0 ? this.votosB * 100.0 / this.total : 0;
+	}
+
+	public void increaseB() {
+		++this.votosB;
+		++this.total;
+		this.porcentajeA = this.total > 0 ? this.votosA * 100.0 / this.total : 0;
+		this.porcentajeB = this.total > 0 ? this.votosB * 100.0 / this.total : 0;
+	}
+
+	public String getPregunta() {
+		return this.pregunta;
+	}
+
+	public int getVotosA() {
+		return this.votosA;
+	}
+
+	public int getVotosB() {
+		return this.votosB;
+	}
+
+	public int getTotal() {
+		return this.total;
+	}
+
+	public double getPorcentajeA() {
+		return this.porcentajeA;
+	}
+
+	public double getPorcentajeB() {
+		return this.porcentajeB;
+	}
+
+}
---
+++ src/ej01_encuesta/Main.java
@@ -0,0 +1,38 @@
+package ej01_encuesta;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Encuesta enc = new Encuesta("Pregunta");
+		
+		System.out.println(enc.getPorcentajeA());
+		System.out.println(enc.getPorcentajeB());
+		
+		enc.increaseA();
+		System.out.println(enc.getTotal());
+		System.out.println(enc.getPorcentajeA());
+		System.out.println(enc.getPorcentajeB());
+		
+		enc.increaseB();
+		System.out.println(enc.getTotal());
+		System.out.println(enc.getPorcentajeA());
+		System.out.println(enc.getPorcentajeB());
+		
+		enc.increaseA();
+		System.out.println(enc.getTotal());
+		System.out.println(enc.getPorcentajeA());
+		System.out.println(enc.getPorcentajeB());
+		
+		enc.increaseA();
+		System.out.println(enc.getTotal());
+		System.out.println(enc.getPorcentajeA());
+		System.out.println(enc.getPorcentajeB());
+		
+		enc.increaseB();
+		System.out.println(enc.getTotal());
+		System.out.println(enc.getPorcentajeA());
+		System.out.println(enc.getPorcentajeB());
+	}
+
+}
